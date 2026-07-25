a6779343e4edade52790d9dc8f9f051324fef88c
SebCaira <90308111+SebCaira@users.noreply.github.com>
2026-07-25 11:43:01 +0200

    Delete strikr-app directory

--- strikr-app/src/lib/avatarColor.ts
+++
@@ -1,7 +0,0 @@
-const AVATAR_COLORS = ['#ffe66b', '#a8f5c6', '#7a2b52', '#2b3ff2', '#2a6f4d', '#ffcae0', '#c9d8ff'];
-
-export function avatarColor(id: string): string {
-  let h = 0;
-  for (let i = 0; i < id.length; i++) h = (h * 31 + id.charCodeAt(i)) & 0xffff;
-  return AVATAR_COLORS[h % AVATAR_COLORS.length];
-}
